1ec43fbb0dd7a949fdafbf8ab091e4e6f54c004b
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-04-28 22:30:33 +0200

    version 3, objects

--- js/toDo.js
+++ js/toDo.js
@@ -1,20 +1,18 @@
-var toDos = ['item1', 'item2', 'item3']
-
-// It should have a function to display toDos
-function displayTodos() {
-	console.log('My Todos:',toDos);
-}
-// It should have a function to add toDos
-function addTodo(toDo) {
-	toDos.push(toDo);
-	displayTodos();
-}
-// It should have a function to edit toDos
-function changeTodo(position, newValue) {
-	toDos[position] = newValue;
-}
-// It should have a function to delete toDos
-function deleteTodo(position) {
-	toDos.splice(position, 1);
-	displayTodos();
-}
+var toDoList = {
+	toDos: ['item1', 'item2', 'item3'],
+	displayToDos() {
+	console.log('My Todos:', this.toDos);
+  },
+  addToDo: function(toDo) {
+  	this.toDos.push(toDo);
+  	this.displayToDos();
+  },
+  changeTodo: function(position, newValue) {
+  	this.toDos[position] = newValue;
+  	this.displayToDos();
+  },
+  deleteToDo: function(position) {
+  	this.toDos.splice(position, 1);
+  	this.displayToDos();
+  }
+};
