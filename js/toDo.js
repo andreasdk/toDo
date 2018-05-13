62a6b209c8607280e4e29e8bea3aade4ca0e360f
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-05-13 22:07:49 +0200

    toggleAll if all true - make false, else make all true

--- js/toDo.js
+++ js/toDo.js
@@ -35,5 +35,28 @@ var toDoList = {
   	var toDo = this.toDos[position];
   	toDo.completed = !toDo.completed;
   	this.displayToDos();
+  },
+  toggleAll : function() {
+  	var totalToDos = this.toDos.length;
+  	var completedToDos = 0;
+
+  	// Get number of compelted toDos
+  	for (var i = 0; i < totalToDos; i++){
+  		if (this.toDos[i].completed === true) {
+  			completedToDos++;
+  		}
+  	}
+
+  	// If everything's true, make everything false
+  	if (completedToDos === totalToDos) {
+  	for (var i = 0; i < totalToDos; i++) {
+  		this.toDos[i].completed = false;
+  	  } 
+  	} else {
+  	  for (var i = 0; i < totalToDos; i++) {
+  	  	this.toDos[i].completed = true;
+  	  	}
+  	  }
+  	  this.displayToDos();
   }
 };
