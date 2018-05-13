b5624e41d3145c6f6d15cb09bbff3cabbaa98a5c
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-05-13 13:58:24 +0200

    displayToDos show ToDoText, displayToDos displays if .toDos is empty, displayToDos shows if completed

--- js/toDo.js
+++ js/toDo.js
@@ -1,7 +1,20 @@
 var toDoList = {
 	toDos: [],
 	displayToDos: function() {
-	console.log('My Todos:', this.toDos);
+		if (this.toDos.length === 0) {
+			console.log('Your todo list is empty!');
+		} else {
+			console.log('My Todos:');
+			for (var i = 0; i < this.toDos.length; i++) {
+			 console.log(this.toDos[i].toDoText);
+
+			 if(this.toDos[i].completed === true){
+			   console.log('(x)', this.toDos[i].toDoText);
+			 } else {
+			   console.log('( )', this.toDos[i].toDoText);
+			 }
+		}
+	}
   },
   addToDo: function(toDoText) {
   	this.toDos.push({
