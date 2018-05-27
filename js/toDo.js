3acf46d3a02965a3ece573ec47682b95f0de278e
Andrew Kennedy <andrew.kennedy27@outlook.com>
2018-05-27 20:54:37 +0200

    displaying toDos on DOM, removed redundant displayToDo function

--- js/toDo.js
+++ js/toDo.js
@@ -1,40 +1,20 @@
 var toDoList = {
 	toDos: [],
-	displayToDos: function() {
-		if (this.toDos.length === 0) {
-			console.log('Your todo list is empty!');
-		} else {
-			console.log('My Todos:');
-			for (var i = 0; i < this.toDos.length; i++) {
-			 console.log(this.toDos[i].toDoText);
-
-			 if(this.toDos[i].completed === true){
-			   console.log('(x)', this.toDos[i].toDoText);
-			 } else {
-			   console.log('( )', this.toDos[i].toDoText);
-			 }
-		}
-	}
-  },
   addToDo: function(toDoText) {
   	this.toDos.push({
   		toDoText: toDoText,
   		completed: false
   	});
-  	this.displayToDos();
   },
   changeToDo: function(position, toDoText) {
   	this.toDos[position].toDoText = toDoText;
-  	this.displayToDos();
   },
   deleteToDo: function(position) {
   	this.toDos.splice(position, 1);
-  	this.displayToDos();
   },
   toggleCompleted : function(position) {
   	var toDo = this.toDos[position];
   	toDo.completed = !toDo.completed;
-  	this.displayToDos();
   },
   toggleAll : function() {
   	var totalToDos = this.toDos.length;
@@ -57,7 +37,6 @@ var toDoList = {
   	  	this.toDos[i].completed = true;
   	  	}
   	  }
-  	  this.displayToDos();
   }
 };
 
